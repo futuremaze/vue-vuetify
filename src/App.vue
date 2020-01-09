e37875ae5a0243fe4ce6815f5654e8b737a1c931
futuremaze <future.maze@gmail.com>
2020-01-09 15:07:18 +0900

    フッターの追加

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <template>
   <v-app>
+    <!-- color: 背景色指定 primary: デフォルト(水色) -->
+    <!-- dark: 文字色が白に -->
+    <!-- app: 高さの自動調整 -->
     <v-app-bar color="primary" dark app>
       <v-toolbar-title>Vuetify</v-toolbar-title>
     </v-app-bar>
+    <!-- color: 背景色指定 primary: デフォルト(水色) -->
+    <!-- dark: 文字色が白に -->
+    <!-- app: 高さの自動調整 -->
+    <v-footer color="primary" dark app>
+      Vuetify
+    </v-footer>
   </v-app>
 </template>
 
