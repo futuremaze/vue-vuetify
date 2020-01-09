8517758fc29535c4c78087a40720aa4ed6b4901e
futuremaze <future.maze@gmail.com>
2020-01-09 15:12:46 +0900

    ナビゲーションメニューの実装

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
   <v-app>
+    <!-- app: 高さの自動調整 -->
+    <!-- clipped: ナビゲーションメニューをナビゲーションバーの下に表示する -->
+    <v-navigation-drawer app v-model="drawer" clipped>
+      Navigation Lists
+    </v-navigation-drawer>
     <!-- color: 背景色指定 primary: デフォルト(水色) -->
     <!-- dark: 文字色が白に -->
     <!-- app: 高さの自動調整 -->
-    <v-app-bar color="primary" dark app>
+    <!-- clipped-left: ナビゲーションメニューをナビゲーションバーの下に表示する -->
+    <v-app-bar color="primary" dark app clipped-left>
+      <!-- v-app-bar-nav-icon: ハンバーガーメニューアイコン -->
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title>Vuetify</v-toolbar-title>
     </v-app-bar>
     <!-- color: 背景色指定 primary: デフォルト(水色) -->
@@ -21,6 +29,7 @@ export default {
 
   data: () => ({
     //
+    drawer: null
   })
 };
 </script>
