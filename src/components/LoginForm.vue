dfb727d8db4a319c2de53a4b51bb2296afb3814c
futuremaze <future.maze@gmail.com>
2020-01-09 14:37:37 +0900

    フォームにアイコンを追加

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -5,8 +5,8 @@
     </v-card-title>
     <v-card-text>
       <v-form>
-        <v-text-field label="ユーザー名" />
-        <v-text-field type="password" label="パスワード" />
+        <v-text-field type="text" prepend-icon="mdi-account-circle" label="ユーザー名" />
+        <v-text-field type="password" prepend-icon="mdi-lock" append-icon="mdi-eye-off" label="パスワード" />
         <v-card-actions>
           <v-btn>ログイン</v-btn>
         </v-card-actions>
