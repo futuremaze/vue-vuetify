450a592175592a50997e1880e86232b48a187c08
futuremaze <future.maze@gmail.com>
2020-01-09 14:41:38 +0900

    フォームの味付け* パスワードフォームの後ろにアイコンを追加* ログインボタンの色を変更* フォームサイズを変更

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,14 +1,19 @@
 <template>
-  <v-card>
+  <v-card width="30%" class="mx-auto mt-5">
     <v-card-title>
       <h1 class="display-1">ログイン</h1>
     </v-card-title>
     <v-card-text>
       <v-form>
         <v-text-field type="text" prepend-icon="mdi-account-circle" label="ユーザー名" />
-        <v-text-field type="password" prepend-icon="mdi-lock" append-icon="mdi-eye-off" label="パスワード" />
+        <v-text-field
+          type="password"
+          prepend-icon="mdi-lock"
+          append-icon="mdi-eye-off"
+          label="パスワード"
+        />
         <v-card-actions>
-          <v-btn>ログイン</v-btn>
+          <v-btn class="primary">ログイン</v-btn>
         </v-card-actions>
       </v-form>
     </v-card-text>
