af7f51a3b234c8a358c1415224415570aeb49cd1
futuremaze <future.maze@gmail.com>
2020-01-09 14:32:43 +0900

    入力フォームの設置

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,7 +1,13 @@
 <template>
-    <v-card>
-        <v-card-title>
-            <h1 class="display-1">ログイン</h1>
-        </v-card-title>
-    </v-card>
+  <v-card>
+    <v-card-title>
+      <h1 class="display-1">ログイン</h1>
+    </v-card-title>
+    <v-card-text>
+      <v-form>
+        <v-text-field label="ユーザー名" />
+        <v-text-field type="password" label="パスワード" />
+      </v-form>
+    </v-card-text>
+  </v-card>
 </template>
